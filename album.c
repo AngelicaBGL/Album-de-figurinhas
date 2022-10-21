b2e0d70cd1230926a4116ad16246b38638d73515
AngelicaBGL <115801184+AngelicaBGL@users.noreply.github.com>
2022-10-21 09:14:54 -0300

    Update album.c

--- album.c
+++ album.c
@@ -2,9 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct{
+  char codigo[10];
+  char titulo[10];
+  char secao[10];
+  char tipo[10];
+}Figurinha;
 
+typedef struct{
+  //figurinhas coladas
+  //figurinhas faltantes
+  //proprirtário
+  //porcentagem do album
+}Album;
+
+//void cadastraFigurinha()
+//void folhearAlbum()
+//void comprarEnvelope()
 
 int main(){
-  
+  //menu
   return 0;
 }
