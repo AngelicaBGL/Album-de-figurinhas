4443181f12f1dde90ca5f817851be7070658622c
vitorviana1011 <116021166+vitorviana1011@users.noreply.github.com>
2022-10-21 06:07:15 -0700

    Update album.c
    coloquei o menu, se precisar de mais coisa avisa

--- album.c
+++ album.c
@@ -24,10 +24,30 @@ typedef struct{
 //void bancaFigurinhas()
 
 int main(){
-  //menu
-  //folhear album
-  //comprar envelope
-  //figurinhas não coladas
-  //dinheiro gasto
+   Album album[2];
+  Figurinha figurinha[2];
+  int menu;
+
+  printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+  printf("\tBEM-VINDO AO ALBUM\n\tO que voce gostaria de fazer?\n");
+  while (menu!=0){
+    printf("\t1. Folhear o album\n\t2. Comprar envelope\n\t3. Mostrar figurinha nao coladas\n\t4. Mostrar dados do album\n\t0. Sair do album\n\tEscolha a opcao: ");
+    scanf("%d", &menu);
+    switch (menu){
+      case 1:
+        Folhear_album();
+        break;
+      case 2:
+        Comprar_Envelope();
+        break;
+      case 3:
+        Figurinhas_Nao_Coladas();
+        break;
+      case 4:
+        Dados_Album();
+    }
+  }
+
+  return 0;
   return 0;
 }
