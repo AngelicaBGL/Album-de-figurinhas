ea736d6c0f4152668ec6ba31c4c387aa2bb82ff3
vitorviana1011 <116021166+vitorviana1011@users.noreply.github.com>
2022-11-30 12:47:36 -0800

    Update depende_do_sistema.c

--- album/depende_do_sistema.c
+++ album/depende_do_sistema.c
@@ -13,19 +13,17 @@ void limpa_buffer(){
    #ifdef linux
       #include <stdio_ext.h>
       __fpurge(stdin);
-   #elif _WIN64
+   #else _WIN64
       setbuf(stdin,NULL);
-   #else
 
    #endif
 }
 void tempo(){
    #ifdef linux
       usleep(5000000);
-   #elif _WIN64
+   #else _WIN64
       #include <Windows.h>
       Sleep(5000);
-   #else
 
    #endif
 }
